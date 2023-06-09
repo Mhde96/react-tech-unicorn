454c34e7f9b0aa6f6ffa8aa55c63149d0c63eeb2
mhde96 <m.mhde96@gmail.com>
2023-06-09 21:59:40 +0400

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navigations } from './routes';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Navigations />
   </React.StrictMode>
 );
 
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomeContainer } from "./containers/HomeContainer";
+import { PlatformLayout } from "./components/layout/PlatformLayout";
+
+export const Navigations = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PlatformLayout />} path="/">
+          <Route element={<HomeContainer />}  />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
