9c63d91f59c38c659ae5bbaa97d39637c8cc1a3a
Zaid-ibrahim12 <zaidibrahim2122@gmail.com>
2023-06-09 22:35:24 +0300

    Add navheader

--- src/components/layout/PlatformLayout.tsx
+++ src/components/layout/PlatformLayout.tsx
@@ -2,11 +2,13 @@ import { Container } from "react-bootstrap";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
+import { NavHeader } from "./NavHeader";
 
 export const PlatformLayout = () => {
   return (
     <>
       <Header />
+      <NavHeader/>
       <Container >
       <Outlet />
       </Container>
