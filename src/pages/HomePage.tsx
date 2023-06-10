c79ce8bc0bedef4de779896442a450ced806be65
Zaid-ibrahim12 <zaidibrahim2122@gmail.com>
2023-06-10 13:03:15 +0300

    discount

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { DiscountSection } from "./home/sections/DiscountSection";
+import { CarouselSectionHome } from "./home/sections/carousel/CarouselSectionHome";
 
 export const HomePage = () => {
   return (
+    <>
     <Container>
-      <div id="home-page-styles">
-        <Col lg={4}>
-          <h1> Sort out Your</h1>
-          <h1> Spring Look</h1>
-          <div>
-            We will help to develop every smallest thing into a big one for your
-            company.
-          </div>
-          <div className="button">
-            <span>Shop</span>
-            <img src="assets/svg/arrow.svg" />
-          </div>
-        </Col>
-        <Col lg={8}>
-          <div className="images">
-            <div />
-            <div />
-          </div>
-        </Col>
-      </div>
-    </Container>
+      
+         
+    <DiscountSection/>
+    <CarouselSectionHome/>
+    </Container> 
+
+    </>
   );
 };
